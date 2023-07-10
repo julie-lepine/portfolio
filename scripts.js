50da6ac770b81232a2e9b4ccf06e3dfd27e895dd
julie-lepine <lepine.julie@live.fr>
2023-07-10 15:43:11 +0200

    alt modifying

--- scripts.js
+++ scripts.js
@@ -6,17 +6,12 @@ menuHamburger.addEventListener('click', () => {
     navLinks.classList.toggle('mobileMenuShow');    
 });
 
-// transition au scroll
-
+// scroll transition
 const ratio = 0.1
 
-// objet qui définit l'ensemble des options de notre intersection observer
 var options = {
-    // élément qui servira de zone d'affichage et qui permettra de détecter si l'élément est visible ou non
     root: null,
-    // marges sur les côtés de la zone d'affichage
     rootMargin : '0px',
-    // à partir de quel moment le système d'intersection est détecté. 1 : entiereté de l'élément doit être visible
     threshold: ratio
 }
 
@@ -30,7 +25,6 @@ const handleIntersect = function (entries, observer) {
 }
 
 const observer = new IntersectionObserver(handleIntersect, options);
-// observer l'élément avec la classe reveal
 document.querySelectorAll('.reveal').forEach(function(r) {
     observer.observe(r);
 })
